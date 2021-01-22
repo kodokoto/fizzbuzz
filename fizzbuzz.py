3fc28f0616ed2ac0bb9ed894c7bde3c8fde88fbd
modii <modiibeats@gmail.com>
2021-01-22 20:07:56 +0100

    added changes

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,10 @@
-def fizzbuzz():
-    return
+# fizzBuzz = lambda multiples, *args: [i if i%multiple==0 else multiples[multiple] for i in range(*args) for multiple in multiples]
+fizzBuzz = lambda multiples, *args: [multiples[multiple] for i in range(*args) for multiple in multiples if i%multiple==0]
 
-print("Hello")
+multiples = {3: 'Fizz', 5: 'Buzz'}
+res = fizzBuzz(multiples, 1, 100)
+print(res)
+
+
+# def getFizzBuzz(multiples, *args):
+#     for i in range(*args):
