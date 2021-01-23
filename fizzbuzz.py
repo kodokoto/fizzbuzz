03bd7bc501bf8b0001558be4fba95029ff0c7072
modii <modiibeats@gmail.com>
2021-01-23 17:49:10 +0100

    now working

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,3 @@
-# fizzBuzz = lambda multiples, *args: [i if i%multiple==0 else multiples[multiple] for i in range(*args) for multiple in multiples]
-fizzBuzz = lambda multiples, *args: [multiples[multiple] for i in range(*args) for multiple in multiples if i%multiple==0]
-
+fizzBuzz = lambda multiples, *args: [(print(''.join([multiples[multiple]*(i%multiple==0) for multiple in multiples if i%multiple==0]) or i)) for i in range(*args)]
 multiples = {3: 'Fizz', 5: 'Buzz'}
-res = fizzBuzz(multiples, 1, 100)
-print(res)
-
-
-# def getFizzBuzz(multiples, *args):
-#     for i in range(*args):
+fizzBuzz(multiples, 1, 100)
