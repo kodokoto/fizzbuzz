8adbcdb55817716055b1fbe09af0b26b118544a7
modii <modiibeats@gmail.com>
2021-01-23 18:02:01 +0100

    added comments

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,7 @@
-fizzBuzz = lambda multiples, *args: [(print(''.join([multiples[multiple]*(i%multiple==0) for multiple in multiples if i%multiple==0]) or i)) for i in range(*args)]
+# This is a modular, one line fizzbuzz function. You are able to easily change the range, the multiples, the words and the amount of both!
+
+# lambda takes in dict of multiples and range      || This part prints each value depending if the key(multiple) goes into i, else print i      || iterate through i by range given
+fizzBuzz = lambda multiples, *rangeArg: [(print(''.join([multiples[multiple]*(i%multiple==0) for multiple in multiples if i%multiple==0]) or i)) for i in range(*rangeArg)]
+# dictionary of multiples, key = multiples, value = word || You can add more than 2!
 multiples = {3: 'Fizz', 5: 'Buzz'}
 fizzBuzz(multiples, 1, 100)
